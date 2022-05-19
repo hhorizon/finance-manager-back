c72563feff4f57a85d95836b3f9008653c0d1a1d
Anatolii Litynskyi <litinskiy.tolik@gmail.com>
2022-05-19 15:27:58 +0300

    created

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const { mkdir } = require("fs/promises");
-
 const app = require("./app");
 const db = require("./config/db");
 
+const PORT = process.env.PORT || 8000;
+
 db.then(() => {
-  app.listen(3001, async () => {
+  app.listen(PORT, async () => {
     await mkdir(process.env.UPLOAD_FOLDER, { recursive: true });
-    console.log("Server running. Use our API on port: 3001");
+    console.log(`Server running. Use our API on port: ${PORT}`);
   });
 }).catch(console.error);
