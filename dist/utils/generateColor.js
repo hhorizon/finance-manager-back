aff8bbc20c1769862fbc2aed10fa94a0f1209a29
Anatolii Litynskiy <litinskiy.tolik@gmail.com>
2023-05-19 22:55:52 +0300

    rm dist

--- dist/utils/generateColor.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generateColor = () => {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    return "#" + randomColor;
-};
-exports.default = generateColor;
