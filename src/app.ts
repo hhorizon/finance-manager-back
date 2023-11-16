5ab0b5837ac0da487dddde4e268d0ebc02a3c20d
Anatolii Litynskiy <litinskiy.tolik@gmail.com>
2023-11-16 23:32:58 +0200

    add cors

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,17 @@ import { limiter } from "./middlewares";
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const corsOriginUrl = process.env.CORS_ORIGIN_URL;
 
 app.use(limiter(15 * 60 * 1000, 100));
 app.use(helmet());
 app.use(logger(formatsLogger));
 app.use(express.static("public"));
-app.use(cors());
+app.use(
+  cors({
+    origin: corsOriginUrl,
+  }),
+);
 app.use(express.json({ limit: 10000 }));
 
 app.use("/api/auth", authRouter);
--- src/routes/statistics/index.ts
+++ src/routes/statistics/index.ts
@@ -1,29 +1,10 @@
 import express from "express";
 
 import { getStatisticsForPeriod } from "../../controllers/statistics";
-import { transactionSchema } from "../../schemas/transactionValidationSchemes";
-import { errorWrapper, guard, validateBody } from "../../middlewares";
+import { errorWrapper, guard } from "../../middlewares";
 
 const router = express.Router();
 
 router.get("/", guard, errorWrapper(getStatisticsForPeriod));
-
-// router.get("/:transactionId", guard, errorWrapper(getTransactionById));
-
-// router.post(
-//   "/",
-//   guard,
-//   validateBody(transactionSchema),
-//   errorWrapper(addTransaction),
-// );
-
-// router.delete("/:transactionId", guard, errorWrapper(removeTransaction));
-
-// router.put(
-//   "/:transactionId",
-//   guard,
-//   validateBody(transactionSchema),
-//   errorWrapper(updateTransaction),
-// );
 
 export default router;
